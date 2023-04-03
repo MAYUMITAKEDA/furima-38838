7eaba5708b78bfcc7ed994bf291d7e78854093f2
MAYUMITAKEDA <mt1291092@gmail.com>
2023-04-03 16:02:14 +0900

    異常系テストコードの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -49,9 +49,21 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
-      it 'passwordが数字のみでは登録できない' do
+      it 'passwordが半角数字のみの場合は登録できない' do
         @user.password = '123456'
         @user.password_confirmation = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid. Include both letters and numbers')
+      end
+      it 'passwordが半角英字のみの場合は登録できない' do
+        @user.password = 'aaaaaa'
+        @user.password_confirmation = 'aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid. Include both letters and numbers')
+      end
+      it 'passwordが全角入力の場合は登録できない' do
+        @user.password = 'ｃ１２３４５'
+        @user.password_confirmation = 'ｃ１２３４５'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is invalid. Include both letters and numbers')
       end
