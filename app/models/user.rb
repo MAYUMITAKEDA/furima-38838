0750b67b3427dc63deb8f876e6af12517be7c701
MAYUMITAKEDA <mt1291092@gmail.com>
2023-04-03 15:30:17 +0900

    userモデルのバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :orders
+  #has_many :items
+  #has_many :orders
   validates :nickname, presence: true
   validates :password,
             format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
   validates :family_name, :first_name,
-            presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters' }
+            presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/, message: 'is invalid. Input full-width characters' }
   validates :family_name_kana, :first_name_kana,
             presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters' }
   validates :birthday, presence: true
