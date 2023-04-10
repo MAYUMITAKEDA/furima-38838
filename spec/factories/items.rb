63b5331a122b9a0cca2dfa89767cd00a3e46d659
MAYUMITAKEDA <mt1291092@gmail.com>
2023-04-10 18:53:23 +0900

    itemモデルの単体テストコードの記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name       { Faker::Lorem.characters(number: 40) }
+    detail          { Faker::Lorem.characters(number: 100) }
+    item_price      { Faker::Number.within(range: 300..9_999_999) }
+    category        { Category.all.sample }
+    condition       { Condition.all.sample }
+    delivery_charge { DeliveryCharge.all.sample }
+    prefecture      { Prefecture.all.sample }
+    delivery_day    { DeliveryDay.all.sample }
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
