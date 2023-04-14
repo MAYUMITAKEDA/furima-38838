c612d2a575aac13ce305a7afb308892757b72c89
MAYUMITAKEDA <mt1291092@gmail.com>
2023-04-14 13:32:36 +0900

    コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit]
-  
-  
-  
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -34,6 +31,15 @@ class ItemsController < ApplicationController
       redirect_to item_path
     else
       render :edit
+    end
+  end
+
+  def destroy
+    @item.destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :show
     end
   end
 
