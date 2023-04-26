aef6c0120f510a75a6a1fbdb12c0a3679be3aab1
MAYUMITAKEDA <mt1291092@gmail.com>
2023-04-26 15:16:14 +0900

    order_delivery_addressモデルの修正
    phone_numberを10桁または11桁の整数のみ保存できるように修正。

--- app/models/order_delivery_address.rb
+++ app/models/order_delivery_address.rb
@@ -6,7 +6,7 @@ class OrderDeliveryAddress
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
     validates :city
     validates :addresses
-    validates :phone_number, format: { with: /\A0\d{10,11}\z/, message: 'is too short' }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is too short' }
     validates :phone_number, numericality: { only_integer: true, message: 'is invalid. Input only number' }
     validates :token
   end
